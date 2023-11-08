eccbe4ce9d8b03bef2978f42ee37588766cab80a
Eduardo Coelho <coelho.edu96@gmail.com>
2023-11-08 19:28:35 -0300

    Aula 2
    . Migração de class para function no App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [showImage, setShowImage] = useState(false);
 
-    this.state = {
-      showImage: false,
-    };
+  const toggle = () => {
+    setShowImage(!showImage);
   }
 
-  toggle = () => {
-    this.setState({
-      showImage: !this.state.showImage,
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <button type='button' onClick={this.toggle}>
-            {this.state.showImage ? 'Esconder' : 'Mostrar'}
-          </button>
-          
-          {/* 
-            Aqui ele interpreta o primeiro bloco (this.state.showImage).
-            Caso o primeiro bloco seja verdadeiro, ele interpreta o segundo.
-            O último bloco interpretado é o que é retornado.
-            No caso, a imagem é exibida.
-          */}
-          {this.state.showImage && (
-            <img src={logo} className="App-logo" alt="logo" />
-          )}
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button type='button' onClick={toggle}>
+          {showImage ? 'Esconder' : 'Mostrar'}
+        </button>
+        
+        {/* 
+          Aqui ele interpreta o primeiro bloco (this.state.showImage).
+          Caso o primeiro bloco seja verdadeiro, ele interpreta o segundo.
+          O último bloco interpretado é o que é retornado.
+          No caso, a imagem é exibida.
+        */}
+        {showImage && (
+          <img src={logo} className="App-logo" alt="logo" />
+        )}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
